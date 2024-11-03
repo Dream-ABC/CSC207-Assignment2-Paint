c27a51b4d9982989af13a9dc83e6db84578af80e
wenjess1 <jessica.wen@mail.utoronto.ca>
2024-11-02 20:18:55 -0400

    oval eraser done

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/Oval.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/Oval.java
@@ -5,6 +5,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
 
+import java.util.ArrayList;
+
 public class Oval implements Shape {
     private Point origin;
     private Point topLeft;
@@ -70,9 +72,32 @@ public class Oval implements Shape {
 
     @Override
     public boolean overlaps(Eraser eraser) {
-        Rectangle e = new Rectangle(eraser.getCentre().x+(eraser.getDimension()/2.0), eraser.getCentre().y+(eraser.getDimension()/2.0), eraser.getDimension(), eraser.getDimension());
-        Ellipse o = new Ellipse(this.topLeft.x-this.width,this.topLeft.y-this.height, this.width, this.height);
-        return e.getBoundsInParent().intersects(o.getBoundsInParent());
+        double h = this.topLeft.x+this.width/2.0;
+        double k = this.topLeft.y+this.height/2.0;
+        double a, b;
+        if (this.height > this.width){
+            a = this.height/2.0;
+            b = this.width/2.0;
+        }
+        else{
+            a = this.width/2.0;
+            b = this.height/2.0;
+        }
+        double leftX = eraser.getCentre().x-(eraser.getDimension()/2.0);
+        double rightX = eraser.getCentre().x+(eraser.getDimension()/2.0);
+        double topY = eraser.getCentre().y-(eraser.getDimension()/2.0);
+        double bottomY = eraser.getCentre().y+(eraser.getDimension()/2.0);
+        ArrayList<Point> allPoints = new ArrayList<Point>();
+        allPoints.add(new Point(leftX, topY));
+        allPoints.add(new Point(leftX, bottomY));
+        allPoints.add(new Point(rightX, topY));
+        allPoints.add(new Point(rightX, bottomY));
+        for (Point point : allPoints) {
+            if (Math.pow((point.x-h)/a, 2) + Math.pow((point.y-k)/b, 2) <= 1){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
