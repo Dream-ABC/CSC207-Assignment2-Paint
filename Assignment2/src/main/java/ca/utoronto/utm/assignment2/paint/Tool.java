56f030c50c58340bfba473a69a6f6ce403dacada
wenjess1 <jessica.wen@mail.utoronto.ca>
2024-11-10 08:12:13 -0500

    interfact javadoc

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/Tool.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/Tool.java
@@ -3,7 +3,7 @@ package ca.utoronto.utm.assignment2.paint;
 import javafx.scene.canvas.GraphicsContext;
 
 /**
- * An interface for all tools the application has.
+ * An interface for all tools the Paint application has.
  */
 public interface Tool {
     Point getTopLeft();
