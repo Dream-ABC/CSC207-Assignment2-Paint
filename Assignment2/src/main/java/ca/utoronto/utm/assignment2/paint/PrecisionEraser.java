c2511a80372d58a5ee411b4b1d033df264a89f6b
hingor13 <dipal.hingorani@mail.utoronto.ca>
2024-11-10 12:27:55 -0500

    finished BUG4.488

--- Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/PrecisionEraser.java
+++ Assignment2/src/main/java/ca/utoronto/utm/assignment2/paint/PrecisionEraser.java
@@ -107,15 +107,6 @@ public class PrecisionEraser implements Shape {
      */
     @Override
     public void display(GraphicsContext g2d) {
-//        for (Point p : points) {
-//            g2d.setLineWidth(1);
-//            g2d.setStroke(this.color);
-//            g2d.setLineDashes(5, 3);
-//            g2d.clearRect(p.x - dimension / 2.0, p.y - dimension / 2.0, this.dimension, this.dimension);
-//            g2d.strokeRect(this.topLeft.x - dimension / 2.0, this.topLeft.y - dimension / 2.0,
-//                    this.dimension, this.dimension);
-//        }
-
         g2d.setLineDashes();
 
         g2d.setLineWidth(1);
